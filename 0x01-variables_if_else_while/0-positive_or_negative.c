da92d48ab9c4d25be8d955deb50c031cd8d6c974
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2022-09-09 19:20:39 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,32 @@
-#include <stdio.h>    
- 
-/**
-* main - Entry point
-* Return: Always 0 (Success)
-*/
-int main()                            
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
 {
-int age;                          
-	   
-printf( "n: " ); 
-scanf( "%d", &n);                
-if ( n> 0 ) {               
-printf ("is positive\n" ); 
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive", n)
+	}
+	if (n == 0)
+	{
+		printf("%d is zero", n)
+	}
+	else
+	{
+		printf("%d is negative", n)
+	}
+	printf("\n")
+	return (0);
+
 }
-else if ( n== 0 ) {         
-printf( "is zero\n" );       
-}
-else {
-printf( "is negative\n" );    
-}
-return n;			       
-}
-gcc 0-positive_or_negative.c -o 0-positive_or_negative.c

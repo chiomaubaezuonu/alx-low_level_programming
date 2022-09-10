91c8c92c23a40564d16b51928345b8d65894b009
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2022-09-09 17:15:38 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,23 @@
-#include<stdio.h>
-
+#include <stdio.h>    
+ 
 /**
 * main - Entry point
-*Return: Always 0 (Success)
+* Return: Always 0 (Success)
 */
-void main() {
-int num;
-printf("Input a number :");
-scanf("%d", &num);
-if (num >= 0)
-printf("%d is a positive number \n", num);
-else
-printf("%d is a negative number \n", num);
+int main()                            
+{
+int age;                          
+	   
+printf( "n: " ); 
+scanf( "%d", &n);                
+if ( n> 0 ) {               
+printf ("is positive\n" ); 
+}
+else if ( n== 0 ) {         
+printf( "is zero\n" );       
+}
+else {
+printf( "is negative\n" );    
+}
+return 0;			       
 }

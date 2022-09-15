92dad514e1c37b098d720a2cea85a388a21ecb61
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2022-09-15 15:04:12 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,31 @@
+/**
+ *
+ *C program to check whether a character is uppercase or lowercase 
+ *
+ */
+
+
+
+#include <stdio.h>
+
+
+
+int main()
+
+{
+
+   	char c;
+
+  	if(c >= 'A' && c <= 'Z')
+		{
+	            printf("'%c' 1", c);
+	        }
+	else if(c >= 'a' && c <= 'z')
+
+	        {
+	 	    printf("'%c' 0", ch);
+		}
+
+		return 0;
+
+}

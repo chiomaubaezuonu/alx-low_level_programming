3caa9cafb0e304ccb000e5fcc42a70a22de2558f
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2022-09-16 19:01:20 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int no;
+
+	for (no = 0; no <= 9; no++)
+		if (no != 2 && no !=4)
+		_putchar(no + '0');
+	_putchar(10);
+}

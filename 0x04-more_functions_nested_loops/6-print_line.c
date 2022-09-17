a7ec2efdf4bd6bf2229f6d005419ff9e277e39fb
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2022-09-16 19:59:55 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: number of lines
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int no;
+
+	for (no = 0; no <= n; no++)
+		_putchar('_');
+	_putchar(10);
+}

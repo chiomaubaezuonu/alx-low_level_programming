bd9b52b184ac4c1759b9f81004c074539671d715
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2022-09-22 22:11:44 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,32 @@
 #include "main.h"
 
 /**
- * _strcmp - a function that compares two strings.
- * @s1: an input string
- * @s2: an input string
- * Return: The difference s1 - s2
+ * _strncpy - a function that copys the strings.
+ * @dest: an input string
+ * @src: an input string
+ * @n: an input integer
+ * Return: A pointer to the resulting string
  */
-int _strcmp(char *s1, char *s2)
+char *_strncpy(char *dest, char *src, int n)
 {
-	while (*s1 && *s2)
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
 	{
-	if (*s1 != *s2)
-		return (*s1 - *s2);
-	s1++, s2++;
+	srclen++;
+	src++;
 	}
-return (*s1 - *s2);
+
+	srclen++;
+
+	if (n > srclen)
+	n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+	*dest++ = *src++;
+
+	return (temp);
 }

2f33c0ebd3806baf4c91169a024fea9061db9468
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2022-10-03 10:15:15 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 
 /**
  * main - a program that prints its name, followed by a new line
- * argc: arguement count
+ * argc: argument count
  * @argv: argument value, a strings that come after calling function
  * Return: Always 0 (Success)
  */

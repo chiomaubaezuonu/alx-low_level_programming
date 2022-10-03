14f72b182b170daa911b5b01a8ead72c756a5d0b
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2022-10-03 00:03:11 -0700

    argc

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,11 +2,12 @@
 int is_divisible(int num, int div);
 
 /**
- * is_prime_number – A function that checks if a number is prime.
+ * isprime_number – 'A function that checks if a number is prime.'
  * @n: an input integer
  * Return: 1 if n is prime or 0 in otherwise
  */
 int is_prime_number(int n)
+
 {
 	int div = 2;
 
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-readme
+argc

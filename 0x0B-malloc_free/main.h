d010028b1ff0f1dde3738f654d574da34a0b692a
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2022-10-05 14:29:49 -0700

    main

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,11 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File: main.h
+ * Auth: Alagwu David
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x0B-malloc_free directory.
+ */
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
@@ -6,3 +13,4 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+#endif
